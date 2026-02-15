2053bf612f2c7cd13aea4aecffa61cbd3691b8ac
Fareed_Osman <fareed.osman152@gmail.com>
2026-02-15 14:52:44 +0200

    First sorting algorithm implementation => Selection Sorting

--- DataStructureAndAlgorithm/Algorithm/Program.cs
+++ DataStructureAndAlgorithm/Algorithm/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Algorithm;
+﻿using Algorithm.Sorting.SelectionSorting;
+
+namespace Algorithm;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<int> xList = [7, 2, 0, 4, 8, 1, 4, 3];
+        PrintList(xList);
+        Console.WriteLine("=============");
+        SelectionSortingService<int>.Sort(xList);
+        PrintList(xList);
+        
+    }
+
+    public static void PrintList(List<int> list)
+    {
+        foreach (var i in list)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
